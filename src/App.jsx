13b9617bf4853749f8ed592f903ef78d243c7fc0
Bobby Naluz <bnaluz9@gmail.com>
2024-05-13 20:49:12 -0400

    add:form inputs

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
+import Form from './components/Form';
+
 function App() {
-  return <h1> Hello from App </h1>;
+  return <Form />;
 }
 
 export default App;
---
+++ src/components/Form.jsx
@@ -0,0 +1,119 @@
+import { useState } from 'react';
+
+const Form = () => {
+  const [formData, setFormData] = useState({
+    name: '',
+    email: '',
+    phone: '',
+    phoneType: '',
+    staff: false,
+    bio: '',
+    optedIn: false,
+  });
+
+  const handleChange = (event) => {
+    const { name, value, type, checked } = event.target;
+    setFormData((prev) => ({
+      ...prev,
+      [name]: type === 'checkbox' ? checked : value,
+    }));
+  };
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+    console.log(formData);
+  };
+
+  return (
+    <div>
+      <form onSubmit={submitHandler}>
+        <p>
+          <label>Name</label>
+          <input
+            type="text"
+            name="name"
+            placeholder="name"
+            onChange={handleChange}
+            value={formData.name}
+          ></input>
+        </p>
+        <p>
+          <label>Email</label>
+          <input
+            type="email"
+            name="email"
+            placeholder="email"
+            value={formData.email}
+            onChange={handleChange}
+          ></input>
+        </p>
+        <p>
+          <label>Phone</label>
+          <input
+            type="text"
+            name="phone"
+            placeholder="phone number"
+            value={formData.phone}
+            onChange={handleChange}
+          ></input>
+        </p>
+        <p>
+          <label>Phone Type:</label>
+          <select
+            name="phoneType"
+            value={formData.phoneType}
+            onChange={handleChange}
+          >
+            <option value="">Select a type</option>
+            <option value="home">Home</option>
+            <option value="work">Work</option>
+            <option value="mobile">Mobile</option>
+          </select>
+        </p>
+        <p>
+          <label>
+            Staff:
+            <input
+              type="radio"
+              name="staffRole"
+              value="instructor"
+              onChange={handleChange}
+            />{' '}
+            Instructor
+          </label>
+          <label>
+            <input
+              type="radio"
+              name="staffRole"
+              value="student"
+              onChange={handleChange}
+            />
+            Student
+          </label>
+        </p>
+        <p>
+          <label>Bio</label>
+          <textarea
+            value={formData.bio}
+            onChange={handleChange}
+            name="bio"
+            placeholder="A quick bio about yourself."
+          ></textarea>
+        </p>
+
+        <p>
+          <label>Sign up for email notifications:</label>
+          <input
+            type="checkbox"
+            name="optedIn"
+            checked={formData.optedIn == true}
+            onChange={handleChange}
+          ></input>
+        </p>
+        <button type="submit">Submit</button>
+      </form>
+    </div>
+  );
+};
+
+export default Form;
